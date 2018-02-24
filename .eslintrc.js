6724c8819f7723d4d75df91a631197c9afe8ea50
Cavan Wagg <waggx002@gmail.com>
2018-02-24 14:14:47 -0600

    Rock, Paper, Scissors functional game using if statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 //This is a eslint configuration file
 
-module.exports = { 'extends': 'airbnb-base' };
+module.exports = {
+    'extends': 'airbnb-base',
+    'rules': {
+      'no-plusplus': ['none'],
+      'allowForLoopAfterthoughts': true
+    }
+  }
+
+
---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// const josh = {
+//     name: 'Josh',
+//     age: 33,
+//     happy: true,
+//     hairColor: 'red'
+// };
+
+// const wade = {
+//     name: 'Wade',
+//     age: 35,
+//     happy: false,
+//     hairColor: 'blonde'
+// }
+// const majid = {
+//     name: 'Majid',
+//     age: 26,
+//     happy: true,
+//     hairColor: 'brown'
+// }
+
+// const studentArray = [josh, wade, majid];
+
+// console.log(josh, wade, majid );
+//  for (let i = 0; i < studentArray.length; i++) {
+//     console.log(`Hello, ${studentArray[i].name}. I heard you are ${studentArray[i].age}. Why are you so ${ studentArray[i].happy ? 'happy' : 'unhappy'}?`);
+//  }
+
+
+
